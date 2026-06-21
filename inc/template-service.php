23d76355b7d49769f7477b8b0df09ef672423443
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2026-06-20 22:08:54 -0400

    Add files via upload

--- inc/template-service.php
+++ inc/template-service.php
@@ -9,13 +9,25 @@ $query = 'SELECT services.servicename, services.servicedescription, services.pri
     WHERE services.id = %d';
 $result = $wpdb->get_results($wpdb->prepare($query, $servicesTable, $usersTable, $service), ARRAY_A);
 ?><main>
-<?php if ($result){
-    echo var_dump($result);
-} else {
-    ?><div class="max-width-500  margin-y-60 centered-x">
-        <h1 class="centered-text margin-bottom-10">Service Not Found</h1>
+    <div class="max-width-700 centered-x margin-y-60">
+    <?php if ($result){
+        ?><h1><?php echo $result[0]['servicename']; ?></h1>
+        <p><?php echo $result[0]['servicedescription']; ?></p>
+        <p><strong>Ballpark pricing: </strong><em><?php echo $result[0]['priceballpark']; ?></em></p>
+        <p><strong>Estimated time to complete: </strong><em><?php echo $result[0]['timeframe']; ?></em></p>
+        <div class="single-service-provider-section">
+            <?php if (is_user_logged_in()){
+                ?><p><strong>Provider: </strong><em><?php echo $result[0]['provider_name']; ?></em></p>
+            <?php } else {
+                ?><p>Only logged in members can view information about service providers.</p>
+                <p>Already a member? <a>Login.</a></p>
+                <p>Not a member yet? <a>Join the cooperative!</a></p>
+            <?php }
+        ?></div>
+    <?php } else {
+        ?><h1 class="centered-text margin-bottom-10">Service Not Found</h1>
         <p>Sorry, we can't find the service you're looking for. <a href="<?php echo esc_url(site_url()) ?>" class="no-wrap">Head home?</a></p>
-    </div>
-<?php }
-?></main>
+    <?php }
+    ?></div>
+</main>
 <?php get_footer();
